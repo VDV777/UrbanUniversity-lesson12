12b03b8bc07d85e0fd0c9181420e5c79c6b7abc4
VDV777 <60316206+VDV777@users.noreply.github.com>
2024-09-13 21:04:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from random import random, randint
+
+
+def getNumberPass(minNumber: int, maxNumber: int) -> str:
+
+    # variant = randint(minNumber, maxNumber)
+    variant = 3
+    print(f'variant: {variant}')
+    password = ''
+
+    for firstNum in range(1, variant):
+
+        for secondNum in range(firstNum + 1, variant):
+
+            if (firstNum + secondNum) % variant == 0:
+
+                password += f'{firstNum} + {secondNum} '
+
+    return password
+
+
+print(getNumberPass(3, 20))
+
+
+
+
+
